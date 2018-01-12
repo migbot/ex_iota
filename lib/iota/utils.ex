c20757db4e449d757c2b7350c3b62c5382bf158b
Miguel Alatorre <this.migbot@gmail.com>
2018-01-12 10:01:56 -0800

    Add tryte<->charlist conversion functions

--- lib/iota/utils.ex
+++ lib/iota/utils.ex
@@ -1,5 +1,7 @@
 defmodule Iota.Utils do
   
+  @tryte_codepoints String.codepoints("9ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
   def is_trytes_of_length?(string, length) when is_binary(string) and is_integer(length) do
     ~r/^[9A-Z]{#{length}}$/
     |> Regex.match?(string)
@@ -11,6 +13,39 @@ defmodule Iota.Utils do
     |> Regex.match?(string)
   end
   def is_trytes?(_), do: false
+
+  def trytes_from_charlist(charlist) when is_list(charlist) do
+    charlist
+    |> Enum.map(&(tryte_from_codepoint/1))
+    |> Enum.join("")
+  end
+  def trytes_from_charlist(_), do: ""
+
+  def trytes_to_charlist(trytes) do
+    case is_trytes?(trytes) do
+      true ->
+        for <<tryte::binary-2 <- trytes>>, do: tryte_to_codepoint(tryte)
+      false ->
+        []
+    end
+  end
+
+  defp tryte_from_codepoint(codepoint) do
+    first_value = rem(codepoint, 27)
+    second_value = div(codepoint - first_value, 27)
+
+    Enum.at(@tryte_codepoints, first_value) <>
+      Enum.at(@tryte_codepoints, second_value)
+  end
+
+  defp tryte_to_codepoint(tryte) do
+    [first | [second | _tail]] = String.codepoints(tryte)
+    <<tryte_codepoint_index(first) + tryte_codepoint_index(second) * 27>>
+  end
+
+  defp tryte_codepoint_index(tryte_codepoint) do
+    Enum.find_index(@tryte_codepoints, &(&1 == tryte_codepoint))
+  end
 
   def valid_address?(address) when is_binary(address) do
     is_trytes_of_length?(address, 81)
